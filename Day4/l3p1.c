c2b1bafd58663ea656834163135b113ff1a2191d
Arunthathi0809 <arunthathimurugan1979@gmail.com>
2025-12-28 15:40:44 +0000

    day4

---
+++ Day4/l3p1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,a;
+    printf("Enter the number: ");
+    scanf("%d", &x);
+    if(x==50)
+     print("success");
+    else
+     print ("failure");
+    return 0;
+}
