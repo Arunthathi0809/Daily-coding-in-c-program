a1610c2e043912f04c2c29bb54b63594b36b19fc
Arunthathi0809 <arunthathimurugan1979@gmail.com>
2026-01-04 15:13:52 +0000

    Day 8

--- Day 7/l4p11.c
+++ Day 7/l4p11.c
@@ -1,24 +1,21 @@
 #include<stdio.h>
-
 int main()
 {
-    int x = 10;
-    int sum = 0;   // ✅ sum declared
-
-loop:
-    if (x <= 100)
+    int x=10;
+    int sum=0;
+    loop:
+    if (x<=100)
     {
-        if (x % 2 != 0)          // odd numbers
+        if (x%2!=0)
         {
-            if ((x / 10) == 7)   // tens digit is 7
+            if ((x/10)==7)
             {
-                sum = sum + x;
-                printf("x = %d, sum = %d\n", x, sum);
+                sum=sum+x;
+              printf("x=%d ,sum = %d\n",x,sum);  
             }
         }
         x++;
         goto loop;
     }
-
     return 0;
 }
