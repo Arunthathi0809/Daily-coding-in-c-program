376af3389b24ab36572de7299bc70ffe1bde70e4
Arunthathi0809 <arunthathimurugan1979@gmail.com>
2026-03-03 15:16:47 +0000

    Assesment 6

--- Assesment 6/p14.c
+++ Assesment 6/p14.c
@@ -1,24 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int i=1,x,firstdigit,lastdigit,power=1,middle,result,sum=0,temp;
-    printf("enter the number:");
-    scanf("%d",&x);
-    lastdigit=x%10;
+    int i, x, firstdigit, lastdigit;
+    int power = 1, middle, result;
+    int sum = 0, temp;
+    printf("Enter the number: ");
+    scanf("%d", &x);
     temp = x;
-    while(temp>0)
+    lastdigit = x % 10;
+    while(temp > 0)
     {
         sum++;
-        temp=temp/10;
+        temp = temp / 10;
     }
-    if(i=1,i<sum,i++)
+    for(i = 1; i < sum; i++)
     {
-        power=power*10;
+        power = power * 10;
     }
-    firstdigit=x/power;
-    middle=x%power;
-    middle=middle/10;
-    result=(lastdigit*power)+middle*10+firstdigit;
-    printf("the final result is %d",result);
+    firstdigit = x / power;
+    middle = x % power;
+    middle = middle / 10;
+    result = (lastdigit * power) + (middle * 10) + firstdigit;
+    printf("The final result is %d\n", result);
     return 0;
 }
