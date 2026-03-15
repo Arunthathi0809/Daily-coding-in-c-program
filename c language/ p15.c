4e11881633a1b29baf6fc982b548d836b0f09d87
Arunthathi0809 <arunthathimurugan1979@gmail.com>
2026-03-15 05:32:32 +0000

    c language

---
+++ c language/ p15.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    char c[8];
+    scanf("%s",c);
+    printf("%d",c[7]);
+}
---
+++ c language/p11.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char c[7] = "ETALVIS";
+    int i;
+    for(i=0;i<7;i++)
+    {
+        printf("%c",c[i]);
+    }
+}
